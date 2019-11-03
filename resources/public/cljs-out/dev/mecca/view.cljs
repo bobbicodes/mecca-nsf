ca6e3b38e5e4fe1903cc4b465821eb5993b71eb3
porkostomus <porkostomus@mail.com>
2019-11-03 11:51:52 -0800

    init

---
+++ resources/public/cljs-out/dev/mecca/view.cljs
@@ -0,0 +1,25 @@
+(ns ^:figwheel-hooks mecca.view
+  (:require [mecca.subs :as subs]
+            [re-frame.core :as rf :refer [subscribe dispatch]]
+            [mecca.events :as events]
+            [goog.object :as o]
+            [goog.crypt :as crypt]))
+
+(defn mecca []
+  [:div
+   [:h1 "Import NSF file"]
+   [:p]
+   [:div
+    [:input#input
+     {:type "file"
+      :on-change
+      (fn [event]
+        (let [dom  (o/get event "target")
+              file (o/getValueByKeys dom #js ["files" 0])
+              reader (js/FileReader.)]
+          (.readAsArrayBuffer reader file)
+          (set! (.-onload reader)
+                (fn [e]
+                  (dispatch [:file-upload (crypt/byteArrayToHex (js/Uint8Array. (-> e .-target .-result)))])))))}]]
+   [:h2 "Hex dump:"]
+   [:p (str @(subscribe [:file-upload]))]])
